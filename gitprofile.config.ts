3ebb6b54dee768f5214765d12f82dac53e8f1a7a
Baosub <167834629+baosub@users.noreply.github.com>
2024-06-08 09:00:15 -0600

    Update gitprofile.config.ts
    Some skills were added

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -100,6 +100,8 @@ const CONFIG = {
       //'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'Git',
+    'Github',
     'Html',
     'Css',
     'JavaScript',
