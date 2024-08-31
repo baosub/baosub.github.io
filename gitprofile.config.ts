34a4881e48d04ea529126fbaa167ba0aa04a256f
Baosub <167834629+baosub@users.noreply.github.com>
2024-08-30 18:15:32 -0600

    Update gitprofile.config.ts
    Some configs added

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -87,7 +87,7 @@ const CONFIG = {
     dribbble: '',
     behance: '',
     medium: '',
-    dev: '',
+    dev: 'baosub',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
@@ -174,8 +174,8 @@ const CONFIG = {
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'baosub', // to hide blog section, keep it empty
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
